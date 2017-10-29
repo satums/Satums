72772b97859a7106379a3744deabef45d0c97c71
xiaoming <xiaoming@xiaoming-PC>
2017-10-29 18:03:05 +0800

    测试的

--- SpringMVC/src/cn/com/satum/service/server/app/RegisterService.java
+++ SpringMVC/src/cn/com/satum/service/server/app/RegisterService.java
@@ -10,9 +10,19 @@ public class RegisterService implements AppService{
 	public String getInfo(String jsondata){
 		System.out.println("===111111111====="+jsondata);
 		AppBo appBo=new AppBo();
-		List list=appBo.query("select * from sh_user where id='"+jsondata+"'");
-		Map<String,String> map=(Map<String,String>)list.get(0);
-		return "调用接口成功"+map;
+		List list=appBo.query("select max(id) id from sh_user");
+	Map map=(Map)list.get(0);
+	int id=0;
+	id=Integer.valueOf(map.get("id").toString());
+	String pa="";
+		try{
+			appBo.runSQL("INSERT INTO sh_user VALUES ('"+(id+1)+"', '15811137696', '15811137696', '4110357d6bb88fd622c7e08d93dbf4a5', '481162', '', '', '0', '0', '0', '', '谭正彪的家', '0', '0', '1234', '2', '', '2', '2017-07-09 00:21:55', '2017-08-05 17:41:05', null);");
+
+		}catch(Exception e){
+		pa.toString();	
+		}
+		
+		return "调用接口成功"+map+pa;
 	}
 
 }
