43607e18d670aaba1ed43d0acccead99f9e59f0a
yxb <15093051062@163.com>
2017-11-17 23:26:22 +0800

    1

--- SpringMVC/src/cn/com/satum/service/server/satums/test/RegisterTest.java
+++ SpringMVC/src/cn/com/satum/service/server/satums/test/RegisterTest.java
@@ -8,11 +8,11 @@ import cn.com.satum.util.PostStyle;
 public class RegisterTest {
 public static void main(String[] args) {
 	
-	String json="{\"username\":\"Ì·Õý±ë\"}";
-	String url="http://192.168.1.100:8080/SpringMVC/services/SatumService?wsdl";
+	String json="{\"username\":\"ß®ÐÁ²©\"}";
+	String url="http://localhost:8080/SpringMVC/services/SatumService?wsdl";
 	//String urls="http://localhost:8080/SpringMVC/services/SatumService";
 	String service="appService";
-	String method="ConAdd";
+	String method="Register";
 	GetStyle gs=new GetStyle();
 	PostStyle ps=new PostStyle();
 	String gep=gs.getData(url,service,method,json);
