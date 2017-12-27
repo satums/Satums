0bda635483b3e599057c9eb7500c8b9315eac31e
yxb <15093051062@163.com>
2017-12-27 08:00:45 +0800

    联动

--- SpringMVC/src/cn/com/satum/service/server/app/QueryLink.java
+++ SpringMVC/src/cn/com/satum/service/server/app/QueryLink.java
@@ -1,15 +1,56 @@
 package cn.com.satum.service.server.app;
 
-public class QueryLink implements AppService{
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+import org.apache.commons.lang.StringUtils;
 
-	}
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+
+import cn.com.Data.Bo.AppBo;
+
+public class QueryLink implements AppService {
 
 	@Override
 	public String getInfo(String jsondata) {
-		// TODO Auto-generated method stub
+
+		Map<String, Object> resMap = new HashMap<String, Object>();
+		Map<String, Object> reqMap = JSON.parseObject(jsondata);
+
+		String userID = (String) reqMap.get("userId");
+		if (StringUtils.isBlank(userID)) {
+			resMap.put("result", "E");
+			resMap.put("msg", "获取不到用户信息！");
+			JSONObject json = new JSONObject(resMap);
+			return json.toString();
+		}
+
+		// 根据用户id查询联动主表
+		@SuppressWarnings("unchecked")
+		List<Map<String, Object>> linkList = AppBo
+				.query("SELECT scl.id,scl.link_code,scl.`name`,scl.`status` FROM sh_common_link scl WHERE scl.is_del='2' AND scl.user_code="
+						+ userID);
+
+		if (linkList != null && linkList.size() > 0) {
+			for (Map<String, Object> linkMap : linkList) {
+				String likeId = (String) linkMap.get("id");// 联动id
+				String likeCode = (String) linkMap.get("link_code");// 联动code
+				String likeName = (String) linkMap.get("name");// 联动名称
+				String likeStatus = (String) linkMap.get("status");// 联动状态
+																	// 1：关闭；2：打开
+
+				// 根据联动code查询联动条件表
+				
+			}
+		} else {
+			resMap.put("result", "S");
+			resMap.put("msg", "该用户没有添加联动！");
+			JSONObject json = new JSONObject(resMap);
+			return json.toString();
+		}
+
 		return null;
 	}
 
