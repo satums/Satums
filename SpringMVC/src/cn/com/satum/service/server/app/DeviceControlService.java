81f48aa15766c59198207e23b981311d7a5ea969
lwf001 <liwenfly1209@163.com>
2017-12-31 18:15:13 +0800

    设备修改

--- SpringMVC/src/cn/com/satum/service/server/app/DeviceControlService.java
+++ SpringMVC/src/cn/com/satum/service/server/app/DeviceControlService.java
@@ -1,11 +1,20 @@
 package cn.com.satum.service.server.app;
-
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+
+
+
+
+
+
+
+
+import net.sf.json.JSONArray;
+
 import org.apache.commons.lang.StringUtils;
+import org.jboss.mx.util.ObservedObject;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
@@ -27,9 +36,21 @@ public class DeviceControlService implements AppService {
 		if ("add".equals(reqType)) {
 			resStr = addDevice(reqMap);// 添加设备
 		} else if ("update".equals(reqType)) {
-			resStr = updateDevice(reqMap);// 修改设备
+			String type = (String) reqMap.get("type");
+			if("1".equals(type)){
+				resStr = updateDevice(reqMap);// 修改设备
+			}
+			else{
+				resStr = updateDeviceSoft(reqMap);// 修改设备排序
+			}
 		} else if ("delete".equals(reqType)) {
-			resStr = deleteDevice(reqMap);// 删除设备
+			String type = (String) reqMap.get("type");
+			if("1".equals(type)){
+				resStr = deleteCommoDevice(reqMap);// 删除基本设备类型
+			}
+			else{
+				resStr = deleteDevice(reqMap);// 删除控制设备
+			}
 		} else if ("query".equals(reqType)) {
 			String type = (String) reqMap.get("type");
 			if("1".equals(type)){
@@ -75,11 +96,11 @@ public class DeviceControlService implements AppService {
 		@SuppressWarnings("unchecked")
 		List<Map<String, Object>> commonDeviceList = AppBo
 				.query("SELECT * from sh_common_device where name='"+ name +"' AND num = '" + num + "' AND user_code = '" + userCode + "' AND device_type_id = '" + typeId + "' AND is_del='2' ");
-		if (commonDeviceList == null ) {
+		if (commonDeviceList.size()==0) {
+			String soft = getSoft("sh_common_device", userCode);
 			// 插入基础设备表
-			String id = DataUtil.getUUID();
-			AppBo.runSQL("INSERT INTO sh_common_device (id,num,name,user_code,device_type_id) VALUES('" + DataUtil.getUUID()
-					+ "','" + num + "','" + name + "','" + userCode + "','" + typeId + "')");
+			AppBo.runSQL("INSERT INTO sh_common_device (id,num,name,user_code,device_type_id,soft,parent_id) VALUES('" + DataUtil.getUUID()
+					+ "','" + num + "','" + name + "','" + userCode + "','" + typeId + "','" + soft + "','1')");
 		}
 		
 		@SuppressWarnings("unchecked")
@@ -99,7 +120,7 @@ public class DeviceControlService implements AppService {
 		List<Map<String, Object>> deviceTypeList = AppBo
 				.query("SELECT id,name from sh_common_device_type where parent_id = '" + typeId + "'  AND is_del='2' ");
 		
-		if (deviceTypeList == null) {//当控制类型level为最底层时
+		if (deviceTypeList.size()==0) {//当控制类型level为最底层时
 			// 查询设备类型表
 			@SuppressWarnings("unchecked")
 			List<Map<String, Object>> deviceTypeList1 = AppBo
@@ -107,8 +128,9 @@ public class DeviceControlService implements AppService {
 			for (Map<String, Object> deviceTypeMap : deviceTypeList1) {
 				String deviceTypeId = (String) deviceTypeMap.get("id");//设备类型id
 				String deviceTypeName = (String) deviceTypeMap.get("name");//设备类型名称id
-				AppBo.runSQL("insert into sh_device (id,user_code,device_type_id,device_type_name,num,name,device_code) values "
-						+ "('"+DataUtil.getUUID()+"','"+userCode+"','"+deviceTypeId+"','"+deviceTypeName+"','"+num+"','"+deviceTypeName+"','"+DataUtil.getUUID()+"')");
+				String soft = getSoft("sh_device", userCode);
+				AppBo.runSQL("insert into sh_device (id,user_code,device_type_id,device_type_name,num,name,device_code,soft) values "
+						+ "('"+DataUtil.getUUID()+"','"+userCode+"','"+deviceTypeId+"','"+deviceTypeName+"','"+num+"','"+deviceTypeName+"','"+DataUtil.getUUID()+"','"+soft+"')");
 			}
 		}
 		
@@ -116,9 +138,9 @@ public class DeviceControlService implements AppService {
 			
 			String deviceTypeId = (String) deviceTypeMap.get("id");//设备类型id
 			String deviceTypeName = (String) deviceTypeMap.get("name");//设备类型名称id
-			
-			AppBo.runSQL("insert into sh_device (id,user_code,device_type_id,device_type_name,num,name,device_code) values "
-					+ "('"+DataUtil.getUUID()+"','"+userCode+"','"+deviceTypeId+"','"+deviceTypeName+"','"+num+"','"+deviceTypeName+"','"+DataUtil.getUUID()+"')");
+			String soft = getSoft("sh_device", userCode);
+			AppBo.runSQL("insert into sh_device (id,user_code,device_type_id,device_type_name,num,name,device_code,soft) values "
+					+ "('"+DataUtil.getUUID()+"','"+userCode+"','"+deviceTypeId+"','"+deviceTypeName+"','"+num+"','"+deviceTypeName+"','"+DataUtil.getUUID()+"','"+soft+"')");
 		} 
 		
 		
@@ -129,213 +151,49 @@ public class DeviceControlService implements AppService {
 	}
 
 	/**
-	 * 修改联动
+	 * 修改控制设备
 	 */
-
 	public String updateDevice(Map<String, Object> reqMap) {
 
 		Map<String, Object> resMap = new HashMap<String, Object>();
-		String linkCode = (String) reqMap.get("LinkCode");
-		if (StringUtils.isBlank(linkCode)) {
-			resMap.put("result", "E");
-			resMap.put("msg", "获取不到要修改的联动信息！");
-			JSONObject json = new JSONObject(resMap);
-			return json.toString();
+		String userCode = (String) reqMap.get("userCode");
+		String id = (String) reqMap.get("id");
+		String name = (String) reqMap.get("name");
+		
+		if (StringUtils.isNotBlank(name)) {
+			AppBo.runSQL("UPDATE sh_device SET name = '" + name + "' WHERE is_del='2' AND id='" + id + "' AND user_code='" + userCode + "'");
 		}
-
-		// 修改联动主表
-		String linkName = (String) reqMap.get("linkName");
-		if (StringUtils.isNotBlank(linkName)) {
-			AppBo.runSQL("UPDATE sh_common_link SET link_name='" + linkName + "' where link_code='" + linkCode + "'");
-		}
-
-		// 修改联动条件子表
-		@SuppressWarnings("unchecked")
-		List<Map<String, Object>> virList = (List<Map<String, Object>>) reqMap.get("virList");// 获取所有的联动条件
-		if (virList != null && virList.size() > 0) {
-			for (Map<String, Object> virMap : virList) {
-				String virId = (String) virMap.get("virId");// 条件id
-				String conVirType = (String) virMap.get("conVirType");// 操作类型；update：修改；del:删除
-				if (StringUtils.isNotBlank(virId)) {
-					if ("update".equals(conVirType)) {
-						@SuppressWarnings("unchecked")
-						List<Map<String, Object>> virIdList = AppBo
-								.query("SELECT slv.vir_code,slv.vir_type,slv.vir_content,slv.vir_param FROM sh_link_virsub slv where slv.id='"
-										+ virId + "'");
-						String virCode = (String) virIdList.get(0).get("vir_code");// 根据条件id获取条件code
-						String virType = (String) virIdList.get(0).get("vir_type");// 根据条件id获取条件类型
-						String virContent = (String) virIdList.get(0).get("vir_content");// 根据条件id获取条件内容
-						String virParam = (String) virIdList.get(0).get("vir_param");// 根据条件id获取条件参数
-
-						if (StringUtils.isNotBlank((String) virMap.get("virType"))) {
-							virType = (String) virMap.get("virType");// 条件类型（0：变为；1：此时正好）
-						}
-						if (StringUtils.isNotBlank((String) virMap.get("virContent"))) {
-							virContent = (String) virMap.get("virContent");// 条件类型（0：变为；1：此时正好）
-						}
-						@SuppressWarnings("unchecked")
-						Map<String, Object> virParamMap = (Map<String, Object>) virMap.get("virParam");// 条件参数
-						if (!virParamMap.isEmpty()) {
-							// 根据条件code获取条件具体参数，条件不同参数就不同
-							if ("time".equals(virCode)) {// 时间
-								String startTime = (String) virParamMap.get("startTime");// 开始时间
-								if (StringUtils.isNotBlank(startTime)) {
-									virParam.replace(virParam.substring(0, virParam.lastIndexOf("-")), startTime);
-								}
-								String endTime = (String) virParamMap.get("endTime");// 结束时间
-								if (StringUtils.isNotBlank(endTime)) {
-									virParam.replace(
-											virParam.substring(virParam.lastIndexOf("-") + 1, virParam.indexOf(" ")),
-											endTime);
-								}
-								String days = (String) virParamMap.get("days");// 周几
-								if (StringUtils.isNotBlank(days)) {
-									virParam.replace(virParam.substring(virParam.indexOf(" ") + 1, virParam.length()),
-											days);
-								}
-							}
-
-							if ("temp".equals(virCode)) {// 温度
-								String lowTemp = (String) virParamMap.get("lowTemp");// 低温
-								if (StringUtils.isNotBlank(lowTemp)) {
-									virParam.replace(virParam.substring(0, virParam.indexOf(" ")), lowTemp);
-								}
-								String highTemp = (String) virParamMap.get("highTemp");// 高温
-								if (StringUtils.isNotBlank(lowTemp)) {
-									virParam.replace(virParam.substring(virParam.indexOf(" ") + 1, virParam.length()),
-											highTemp);
-								}
-								AppBo.runSQL("UPDATE sh_link_dic SET param_low='" + lowTemp + "',param_high='"
-										+ highTemp + "' where `code`='" + virCode + "'");
-							}
-							if ("damp".equals(virCode)) {// 湿度
-								String lowDamp = (String) virParamMap.get("lowDamp");// 低点
-								if (StringUtils.isNotBlank(lowDamp)) {
-									virParam.replace(virParam.substring(0, virParam.indexOf(" ")), lowDamp);
-								}
-								String highDamp = (String) virParamMap.get("highDamp");// 高点
-								if (StringUtils.isNotBlank(highDamp)) {
-									virParam.replace(virParam.substring(virParam.indexOf(" ") + 1, virParam.length()),
-											highDamp);
-								}
-								AppBo.runSQL("UPDATE sh_link_dic SET param_low='" + lowDamp + "',param_high='"
-										+ highDamp + "' where `code`='" + virCode + "'");
-							}
-							if ("illu".equals(virCode)) {// 光照度
-								String lowIllu = (String) virParamMap.get("lowIllu");// 低点
-								if (StringUtils.isNotBlank(lowIllu)) {
-									virParam.replace(virParam.substring(0, virParam.indexOf(" ")), lowIllu);
-								}
-								String highIllu = (String) virParamMap.get("highIllu");// 高点
-								if (StringUtils.isNotBlank(highIllu)) {
-									virParam.replace(virParam.substring(virParam.indexOf(" ") + 1, virParam.length()),
-											highIllu);
-								}
-								AppBo.runSQL("UPDATE sh_link_dic SET param_low='" + lowIllu + "',param_high='"
-										+ highIllu + "' where `code`='" + virCode + "'");
-							}
-							if ("trig".equals(virCode)) {// 触发
-								String seconds = (String) virParamMap.get("seconds");// 秒数
-								if (StringUtils.isNotBlank(seconds)) {
-									virParam = seconds;
-								}
-								AppBo.runSQL("UPDATE sh_link_dic SET param_low='" + seconds + "',param_high='" + seconds
-										+ "' where `code`='" + virCode + "'");
-							}
-						}
-
-						AppBo.runSQL("UPDATE sh_link_virsub SET vir_type='" + virType + "',vir_content='" + virContent
-								+ "',vir_param='" + virParam + "' where id='" + virId + "'");
-					} else if ("del".equals(conVirType)) {
-						// 删除联动条件子表
-						AppBo.runSQL("UPDATE sh_link_virsub SET is_del='1' WHERE id='" + virId + "'");
-					}
-
-				}
-
-			}
-
-		}
-		// 修改联动设备子表
-		@SuppressWarnings("unchecked")
-		List<Map<String, Object>> deviceList = (List<Map<String, Object>>) reqMap.get("deviceList");// 获取所有在当前联动的设备列表
-		if (deviceList != null && deviceList.size() > 0) {
-			for (Map<String, Object> deviceMap : deviceList) {
-				String deviceId = (String) deviceMap.get("deviceId");
-				String conDeviceType = (String) deviceMap.get("conDeviceType");// 操作类型；update：修改；del:删除
-				if (StringUtils.isNotBlank(deviceId)) {
-					if ("update".equals(conDeviceType)) {
-						@SuppressWarnings("unchecked")
-						List<Map<String, Object>> deviceIdList = AppBo
-								.query("SELECT sld.device_status,sld.device_contime FROM sh_link_devicesub sld where sld.id='"
-										+ deviceId + "'");
-						String deviceStatus = (String) deviceIdList.get(0).get("device_status");// 根据设备id查询设备状态
-						String deviceContime = (String) deviceIdList.get(0).get("device_contime");// 控制时间；0代表立即
-
-						if (StringUtils.isNotBlank((String) deviceMap.get("deviceStatus"))) {
-							deviceStatus = (String) deviceMap.get("deviceStatus");
-						}
-						if (StringUtils.isNotBlank((String) deviceMap.get("deviceContime"))) {
-							deviceContime = (String) deviceMap.get("deviceContime");
-						}
-
-						AppBo.runSQL("UPDATE sh_link_devicesub SET device_status = '" + deviceStatus
-								+ "',device_contime='" + deviceContime + "' where id='" + deviceId + "'");
-					} else if ("del".equals(conDeviceType)) {
-						// 删除联动设备子表
-						AppBo.runSQL("UPDATE sh_link_devicesub SET is_del='1' WHERE id='" + deviceId + "'");
-					}
-
-				}
-			}
-		}
-		// 修改联动情景子表
-		@SuppressWarnings("unchecked")
-		List<Map<String, Object>> sceneList = (List<Map<String, Object>>) reqMap.get("sceneList");// 获取所有在当前联动的情景列表
-		if (sceneList != null && sceneList.size() > 0) {
-			for (Map<String, Object> sceneMap : sceneList) {
-				String sceneId = (String) sceneMap.get("sceneId");
-				String conSceneType = (String) sceneMap.get("conSceneType");// 操作类型；update：修改；del:删除
-				if (StringUtils.isNotBlank(sceneId)) {
-					if ("update".equals(conSceneType)) {
-						@SuppressWarnings("unchecked")
-						List<Map<String, Object>> sceneIdList = AppBo.query(
-								"SELECT scene_status,scene_contime FROM sh_link_scenesub where id='" + sceneId + "'");
-						String sceneStatus = (String) sceneIdList.get(0).get("scene_status");// 根据情景id查询状态
-						String sceneContime = (String) sceneIdList.get(0).get("scene_contime");// 控制时间；0代表立即
-
-						if (StringUtils.isNotBlank((String) sceneMap.get("sceneStatus"))) {
-							sceneStatus = (String) sceneMap.get("sceneStatus");
-						}
-						if (StringUtils.isNotBlank((String) sceneMap.get("sceneContime"))) {
-							sceneContime = (String) sceneMap.get("sceneContime");
-						}
-						AppBo.runSQL("UPDATE sh_link_scenesub SET scene_status='" + sceneStatus + "',scene_contime='"
-								+ sceneContime + "' where id='" + sceneId + "'");
-					} else if ("del".equals(conSceneType)) {
-						// 删除联动情景子表
-						AppBo.runSQL("UPDATE sh_link_scenesub SET is_del='1' WHERE id='" + sceneId + "'");
-					}
-				}
-				String sceneName = (String) sceneMap.get("sceneName");// 情景名称
-				String sceneCode = (String) sceneMap.get("sceneCode");// 情景code
-				String sceneStatus = (String) sceneMap.get("sceneStatus");// 状态
-				String sceneContime = (String) sceneMap.get("sceneContime");// 控制时间；0代表立即
-
-				AppBo.runSQL(
-						"INSERT INTO sh_link_devicesub (id,link_code,scene_name,scene_code,scene_status,scene_contime) VALUES ('"
-								+ DataUtil.getUUID() + "','" + linkCode + "','" + sceneName + "','" + sceneCode + "','"
-								+ sceneStatus + "','" + sceneContime + "')");
-			}
-		}
+		
 		resMap.put("result", "S");
-		resMap.put("msg", "联动修改成功！");
+		resMap.put("msg", "设备修改成功！");
+		JSONObject json = new JSONObject(resMap);
+		return json.toString();
+	}
+	
+	/**
+	 * 修改控制设备排序
+	 */
+	public String updateDeviceSoft(Map<String, Object> reqMap) {
+
+		Map<String, Object> resMap = new HashMap<String, Object>();
+		String userCode = (String) reqMap.get("userCode");
+		@SuppressWarnings("unchecked")
+		List<Map<String,Object>> devices = (List<Map<String, Object>>) reqMap.get("device"); 
+		for(int i = 0;i<devices.size();i++){
+			Map<String,Object> deviceMap=devices.get(i);
+			String id = (String) deviceMap.get("id");   
+			String soft = (String) deviceMap.get("soft"); 
+			AppBo.runSQL("UPDATE sh_device SET soft = '" + soft + "' WHERE is_del='2' AND id='" + id + "' AND user_code='" + userCode + "'");
+		}
+		
+		resMap.put("result", "S");
+		resMap.put("msg", "设备排序修改成功！");
 		JSONObject json = new JSONObject(resMap);
 		return json.toString();
 	}
 
 	/**
-	 * 删除联动
+	 * 删除控制设备
 	 * 
 	 * @param reqMap
 	 * @return
@@ -344,17 +202,33 @@ public class DeviceControlService implements AppService {
 
 		Map<String, Object> resMap = new HashMap<String, Object>();
 
-		String linkCode = (String) reqMap.get("linkCode");
-		// 删除联动条件子表
-		AppBo.runSQL("UPDATE sh_link_virsub SET is_del='1' WHERE link_code='" + linkCode + "'");
-		// 删除联动设备子表
-		AppBo.runSQL("UPDATE sh_link_devicesub SET is_del='1' WHERE link_code='" + linkCode + "'");
-		// 删除联动情景子表
-		AppBo.runSQL("UPDATE sh_link_scenesub SET is_del='1' WHERE link_code='" + linkCode + "'");
-		// 删除联动主表
-		AppBo.runSQL("UPDATE sh_common_link SET is_del='1' WHERE link_code='" + linkCode + "'");
+		String id = (String) reqMap.get("id");
+		//删除控制设备
+		AppBo.runSQL("UPDATE sh_device SET is_del='1' WHERE id='" + id + "'");
 		resMap.put("result", "S");
-		resMap.put("msg", "联动删除成功！");
+		resMap.put("msg", "设备删除成功！");
+		JSONObject json = new JSONObject(resMap);
+		return json.toString();
+
+	}
+	
+	/**
+	 * 删除基础设备
+	 * 
+	 * @param reqMap
+	 * @return
+	 */
+	public String deleteCommoDevice(Map<String, Object> reqMap) {
+
+		Map<String, Object> resMap = new HashMap<String, Object>();
+
+		String num = (String) reqMap.get("num");
+		//删除控制设备
+		AppBo.runSQL("UPDATE sh_device SET is_del='1' WHERE num='" + num + "'");
+		//删除基本设备
+		AppBo.runSQL("UPDATE sh_common_device SET is_del='1' WHERE num='" + num + "'");
+		resMap.put("result", "S");
+		resMap.put("msg", "设备删除成功！");
 		JSONObject json = new JSONObject(resMap);
 		return json.toString();
 
@@ -382,10 +256,10 @@ public class DeviceControlService implements AppService {
 		// 根据parent_id查询设备类别
 		@SuppressWarnings("unchecked")
 		List<Map<String, Object>> deviceList = AppBo
-				.query("SELECT id,name FROM sh_common_device_type WHERE is_del='2' AND parent_id="
-						+ parentId);
+				.query("SELECT id,name FROM sh_common_device_type WHERE is_del='2' AND parent_id='"
+						+ parentId + "' ORDER BY soft ");
 		
-		if (deviceList == null)  {
+		if (deviceList.size()==0)  {
 			resMap.put("result", "S");
 			resMap.put("msg", "未查询到设备类别！");
 			JSONObject json = new JSONObject(resMap);
@@ -421,10 +295,10 @@ public class DeviceControlService implements AppService {
 		// 根据userCode查询设备
 		@SuppressWarnings("unchecked")
 		List<Map<String, Object>> deviceList = AppBo
-				.query("SELECT id,name FROM sh_device WHERE is_del='2' AND user_code="
-						+ userCode);
+				.query("SELECT id,name FROM sh_device WHERE is_del='2' AND user_code='"
+						+ userCode + "' ORDER BY soft " ); 
 		
-		if (deviceList == null)  {
+		if (deviceList.size()==0)  {
 			resMap.put("result", "S");
 			resMap.put("msg", "未查询到设备！");
 			JSONObject json = new JSONObject(resMap);
@@ -436,6 +310,30 @@ public class DeviceControlService implements AppService {
 		resMap.put("data", deviceList);
 		JSONObject json = new JSONObject(resMap);
 		return json.toString();
+
+	}
+	
+	/**
+	 * 查询数据库排序
+	 * @param table
+	 * @param userCode
+	 * @return
+	 */
+	public String getSoft(String table, String userCode) {
+		
+		@SuppressWarnings("unchecked")
+		List<Map<String, Object>> softLists = AppBo
+				.query("SELECT MAX(soft) as soft from " + table + " where is_del='2' AND user_code = '" + userCode + "' ");
+	    Integer soft = 0;
+	    if (softLists != null && softLists.size() > 0) { 
+	    	for (Map<String, Object> softList : softLists) {
+				String softs = (String) softList.get("soft");
+				if(softs != null){					
+					soft = Integer.parseInt(softs) + 1;
+				}
+		    }
+	    }
+		return soft.toString();
 
 	}
 }
