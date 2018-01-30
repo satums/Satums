330b57409944ba69483866fa9fe503c5082132b3
tzb <1418281862@qq.com>
2018-01-30 22:34:05 +0800

    图片下载链接生成

--- SpringMVC/src/cn/com/controller/FileDownload.java
+++ SpringMVC/src/cn/com/controller/FileDownload.java
@@ -32,7 +32,7 @@ public class FileDownload {
 		             HttpServletRequest request, HttpServletResponse response) {
 			String fileName="";
 			String path="";
-				List list=new AppBo().query("select * from sh_common_icon where file_type='"+flag+"' and id='"+id+"'");
+				List list=new AppBo().query("select * from sh_common_icon where file_type='"+flag+"' and id='"+id+"' and is_del='2' order by created_at desc");
 		if(list.size()>0){
 			Map maps=(Map)list.get(0);
 			fileName=(String) maps.get("name_sys");
@@ -88,7 +88,68 @@ public class FileDownload {
 		         }
 		         return null;
 		     }
-
+	@RequestMapping("backImage")	
+public String backImage(@RequestParam("flag") String flag,@RequestParam("userid")String id,
+        HttpServletRequest request, HttpServletResponse response){
+		String fileName="";
+		String path="";
+			List list=new AppBo().query("select * from sh_common_icon where types='"+flag+"' and user_id='"+id+"' and is_del='2' order by created_at desc");
+	if(list.size()>0){
+		Map maps=(Map)list.get(0);
+		fileName=(String) maps.get("name_sys");
+		path=(String) maps.get("url");			
+		try {
+			byte[] b=new BASE64Decoder().decodeBuffer(path);
+			path=new String(b,"utf-8");
+			System.out.println(path);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	         if (fileName != null) {
+	             String realPath = path;
+	             File file = new File(realPath,fileName);		      		            
+	                 response.setContentType("application/force-download");// 设置强制下载不打开
+	                 response.addHeader("Content-Disposition","attachment;fileName=" + fileName);// 设置文件名		               
+	                 byte[] buffer = new byte[1024];
+	                 FileInputStream fis = null;
+	                 BufferedInputStream bis = null;
+	                 try {
+	                     fis = new FileInputStream(file);
+	                     bis = new BufferedInputStream(fis);
+	                     OutputStream os = response.getOutputStream();
+	                     int i = bis.read(buffer);
+	                     while (i != -1) {
+	                         os.write(buffer, 0, i);
+	                         i = bis.read(buffer);
+	                     }
+	                 } catch (Exception e) {
+	                     // TODO: handle exception
+	                     e.printStackTrace();
+	                 } finally {
+	                     if (bis != null) {
+	                         try {
+	                             bis.close();
+	                         } catch (IOException e) {
+	                             // TODO Auto-generated catch block
+	                             e.printStackTrace();
+	                         }
+	                     }
+	                     if (fis != null) {
+	                         try {
+	                             fis.close();
+	                         } catch (IOException e) {
+	                             // TODO Auto-generated catch block
+	                             e.printStackTrace();
+	                         }
+	                     }
+	                 }
+	            
+	         }
+			return null;
+	
+}
 	}
 
 
